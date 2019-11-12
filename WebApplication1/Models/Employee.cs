42340948bd0d5fe40321700d6cb9386995319527
Mefail Baxhaku <mb40346@ubt-uni.net>
2019-11-12 03:33:37 +0100

    All forms validated, paymentperhour added

--- WebApplication1/Models/Employee.cs
+++ WebApplication1/Models/Employee.cs
@@ -12,22 +12,32 @@ namespace WebApplication1.Models
         }
 
         public int Id { get; set; }
-        [Required]
+        [Required (ErrorMessage = "Ju lutem shenoni emrin!")]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "Emri duhet te jete me i madh se 3 shkronja!")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Perdorni vetem shkronja, ju lutem!")]
         public string FirstName { get; set; }
-        [Required]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "Mbiemri duhet te jete me i madh se 3 shkronja!")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Perdorni vetem shkronja, ju lutem!")]
+        [Required (ErrorMessage = "Ju lutem shenoni mbiemrin!")]
         public string LastName { get; set; }
-        [Required]
+        [Required (ErrorMessage = "Ju lutem zgjidhni gjinine!")]
         public string Gender { get; set; }
-        [Required]
+        [Required (ErrorMessage = "Ju lutem shenoni numrin e telefonit!")]
         public int PhoneNumber { get; set; }
-        [Required]
+        [Required (ErrorMessage = "Ju lutem shenoni Qytetin!")]
+        [StringLength(15, MinimumLength = 3)]
+
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Perdorni vetem shkronja, ju lutem!")]
         public string City { get; set; }
-        [Required]
+        [Required (ErrorMessage = "Ju lutem zgjidhni Shtetin!")]
         public string State { get; set; }
-        [Required]
+        [Required (ErrorMessage = "Ju lutem shenoni poziten!")]
         public string Position { get; set; }
-        [Required]
+        [Required (ErrorMessage = "Ju lutem zgjidhni orarin!")]
         public string Schedule { get; set; }
+        [Required (ErrorMessage = "Ju lutem caktoni pagen!")]
+        [RegularExpression(@" ^\d{1, 6}(?:\.\d{0,2})", ErrorMessage = "Perdorni vetem numra,ju lutem!")]
+        public double PaymentPerHour { get; set; }
 
         public virtual ICollection<Attendance> Attendance { get; set; }
     }
