39e6842abe9c802448f347ae04795b2c09b5f1ed
lekcermjani <lekcermjani@hotmail.com>
2019-12-20 01:40:42 +0100

    Kontrata fix, new employ fields, validation,

---
+++ EMS/Migrations/20191220000445_NewFields.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EMS.Migrations
+{
+    public partial class NewFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Banka",
+                table: "Employee",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthday",
+                table: "Employee",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Employee",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<long>(
+                name: "NrBankes",
+                table: "Employee",
+                maxLength: 16,
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "PersonalNumber",
+                table: "Employee",
+                maxLength: 11,
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeRroga",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Position = table.Column<string>(nullable: true),
+                    Paga = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeRroga", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeRroga");
+
+            migrationBuilder.DropColumn(
+                name: "Banka",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "Birthday",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "NrBankes",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "PersonalNumber",
+                table: "Employee");
+        }
+    }
+}
